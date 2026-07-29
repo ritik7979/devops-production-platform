85870cac530549189b40fda37eeece4e7273fb1f
ritik <ritik@gmail.com>
2026-07-29 14:28:43 +0000

    Add Prometheus metrics endpoint

--- application/backend/app.js
+++ application/backend/app.js
@@ -1,14 +1,43 @@
 require("dotenv").config();
 
 const express = require("express");
-
 const cors = require("cors");
+const client = require("prom-client");
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
+
+// Register default metrics
+const register = new client.Registry();
+
+client.collectDefaultMetrics({
+    register
+});
+
+// HTTP Request Counter
+const httpRequestCounter = new client.Counter({
+    name: "http_requests_total",
+    help: "Total number of HTTP requests",
+    labelNames: ["method", "route", "status"]
+});
+
+register.registerMetric(httpRequestCounter);
+
+// Middleware
+app.use((req, res, next) => {
+
+    res.on("finish", () => {
+        httpRequestCounter.inc({
+            method: req.method,
+            route: req.route?.path || req.path,
+            status: res.statusCode
+        });
+    });
+
+    next();
+});
 
 const productRoutes = require("./routes/productRoutes");
 
@@ -20,6 +49,12 @@ app.get("/health", (req, res) => {
         service: "backend",
         timestamp: new Date().toISOString()
     });
+});
+
+// Prometheus endpoint
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", register.contentType);
+    res.end(await register.metrics());
 });
 
 const PORT = process.env.PORT || 5000;
